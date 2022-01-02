117d0d4dbbc9ada78e8f8fe164efda919b550f0e
Nico <nicolasimplant@gmail.com>
2022-01-02 16:53:56 -0500

    End class 9 status code: Ok

--- posts/views.py
+++ posts/views.py
@@ -6,34 +6,45 @@ from datetime import datetime
 
 posts = [
     {
-        'name' : 'Gumball',
-        'user' : 'Isabella',
-        'Timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'https://is2-ssl.mzstatic.com/image/thumb/Purple/v4/d3/9c/1f/d39c1f82-2fc3-1b5f-33bc-025174f487cf/source/256x256bb.jpg',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
-
     {
-        'name' : 'Cowboy Bebbop',
-        'user' : 'Diego',
-        'Timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'http://pngwebicons.com/uploads/anime/ico/anime_icon6679.ico',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
-
     {
-        'name' : 'Ghost in the shell',
-        'user' : 'Nicolás',
-        'Timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'https://styles.redditmedia.com/t5_dxqku/styles/profileIcon_12g7uigwohn71.jpg?width=256&height=256&crop=256:256,smart&s=18557f86cc842f405cb4fcafd91f63facfdc75d7',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
+# def list_posts(request):
+#     """Return a list with existing posts"""
+#     content = []
+#     for post in posts:
+#         content.append("""
+#         <p><strong>{name}</strong></p>
+#         <p><small>{user} - <i>{Timestamp}</i></strong></p>
+#         <figure><img src='{picture}'/></figure>
+#         """.format(**post))
+#     return HttpResponse('<br>'.join(content))
+
 def list_posts(request):
     """Return a list with existing posts"""
-    content = []
-    for post in posts:
-        content.append("""
-        <p><strong>{name}</strong></p>
-        <p><small>{user} - <i>{Timestamp}</i></strong></p>
-        <figure><img src='{picture}'/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts' : posts})
