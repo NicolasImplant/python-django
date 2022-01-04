0d45119686158173d2c9bd562898bc66d91d92c7
Nico <nicolasimplant@gmail.com>
2022-01-04 15:31:48 -0500

    End class 19, status code: ok, add templates on new folder using bootstrap

--- posts/views.py
+++ posts/views.py
@@ -47,4 +47,4 @@ posts = [
 
 def list_posts(request):
     """Return a list with existing posts"""
-    return render(request, 'feed.html', {'posts' : posts})
+    return render(request, 'posts/feed.html', {'posts' : posts})
