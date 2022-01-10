758d8864a299ce291af10b914e672decd6468555
Nico <nicolasimplant@gmail.com>
2022-01-10 14:28:44 -0500

    End class 27, Status code: Ok, Forms validdate

--- users/views.py
+++ users/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
-from django.db.utils import IntegrityError
-
-from django.contrib.auth.models import User
-from users.models import Profile
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 # Create your views here.
 
@@ -63,31 +58,18 @@ def signup(request):
     """Signup View"""
 
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirmation = request.POST['password_confirmation']
-
-        if password != password_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-        
-        try:
-            user = User.objects.create_user(username = username, password = password)
-
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already exist !'})
-
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()        
-
-        return redirect('Login')
-
-    return render(request, 'users/signup.html')
+        form =SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Login')
+    else:
+        form = SignupForm()
+    
+    return render(
+        request = request,
+        template_name = 'users/signup.html',
+        context = {'form': form},
+    )
 
 @login_required
 def logout_view(request):
